c6c1c93940dc9eaefcfd419f48b4362493cc8c39
rishiraj007 <rishiraj@mounttalent.com>
2023-04-11 16:44:41 +0530

    [Prabhu]Add: JSON Inventory Management

---
+++ JSONInventoryManagement/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace JSONInventoryManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("JSON Inventory Management!");
+        }
+    }
+}
