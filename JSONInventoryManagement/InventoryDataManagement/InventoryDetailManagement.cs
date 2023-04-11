4cf8e5c4a2baa61f3ae82b35afc7740c664793ae
rishiraj007 <rishiraj@mounttalent.com>
2023-04-11 16:50:14 +0530

    [Prabhu]Add: Inventory Data Management

---
+++ JSONInventoryManagement/InventoryDataManagement/InventoryDetailManagement.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONInventoryManagement.InventoryDataManagement
+{
+    public class InventoryDetailManagement
+    {
+        public void ReadJSONFile(string file)
+        {
+            var jsondata = File.ReadAllText(file);
+            List<Inventory> inventories = JsonConvert.DeserializeObject<List<Inventory>>(jsondata); //json file data into string data
+            foreach (var data in inventories)
+            {
+                Console.WriteLine("Inventory Name: " + data.Name + "\n" + "Weight: " + data.Weight + "\n" + "Price Per Kg: " + data.PricePerKg + "\n" + "Total Price: " + data.Weight * data.PricePerKg);
+            }
+        }
+    }
+}
--- JSONInventoryManagement/Program.cs
+++ JSONInventoryManagement/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace JSONInventoryManagement
+﻿using JSONInventoryManagement.InventoryDataManagement;
+
+namespace JSONInventoryManagement
 {
     internal class Program
     {
+        static string jsonFilePath = @"C:\Users\91789\OneDrive\Desktop\bridgelabz\InventoryManagement-JSON\JSONInventoryManagement\InventoryDataManagement\Inventory.json";
         static void Main(string[] args)
         {
             Console.WriteLine("JSON Inventory Management!");
+            InventoryDetailManagement inventory = new InventoryDetailManagement();
+            inventory.ReadJSONFile(jsonFilePath);
         }
     }
 }
